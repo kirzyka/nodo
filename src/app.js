9e7e56b53969b728c002afde0145fc94de039618
Anton Kirzyk <kirzyka@gmail.com>
2017-05-17 21:03:27 +0000

    Added MongoDB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 	path = require('path'),
-	app = express();
+	app = express(),
+	mongoose = require('mongoose');
 	
 // Globals	
 global.BASE_PATH = __dirname;	
@@ -17,4 +18,20 @@ app.use(require('./app.routes'));
 // Run
 app.listen(3000, function () {
 	console.log('Nodo::Server listening on port 3000!');
+});
+
+mongoose.connect('mongodb://localhost:27017/nodo');
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+ 
+var User = new Schema({
+    username  : String,
+    id     	  : Number,
+    sex       : String
+});
+
+var myModel = mongoose.model('users', User);
+myModel.find({}, function (err, items) {
+	console.log(items); 
 });
